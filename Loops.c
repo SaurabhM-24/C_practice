1c4006080a4f2a6980a3ab4acf448fe56d0e1762
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-01-06 18:37:20 +0530

    Create Loops.c

---
+++ Loops.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+  int n;
+  printf("Enter the size of list: ");
+  scanf("%d", &n);
+  int arr[n];
+  printf("Enter the elements of the list: ");
+  for(int i=0; i<n; i++){
+    scanf("%d", &arr[i]);
+  }
+  int count=0;
+  for(int i=0; i<n; i++){
+    if(arr[i] % 2 != 0){
+      count++;
+    }
+  }
+    if(count>0){
+        printf("%d", count);
+    }
+    else{
+        printf("No odd number present in the list");
+    }
+  return 0;
+  }
