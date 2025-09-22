45cf686558a4206ca140e1a687974a4f2b67b2c9
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-09-22 22:03:27 +0530

    Add program to check even or odd using bitwise operators

--- Basics_and_introduction.c
+++ Basics_and_introduction.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 
 int main(){
+  
   int n;
   printf("Enter a number: ");
   scanf("%d", &n);
+  
   if(n & 1){
     printf("%d is an odd number", n);
   }    
+  
   else{
     printf("%d is an even number", n);
   }
+  
   return 0;
 }
